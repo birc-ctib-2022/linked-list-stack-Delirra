f86ef7b59088da80cf5139f8a168a7b5aba3240f
Rasmus Freund <rasmus3141@gmail.com>
2022-11-28 10:30:49 +0100

    Implemented single-linked stack with tests

--- src/test_stack.py
+++ src/test_stack.py
@@ -1,6 +1,19 @@
 """Testing our stack implementation."""
+from stack import(Link, Stack)
 
 
-def test_me() -> None:
+def test_stack_isEmpty() -> None:
     """I really hope you test your code."""
-    assert 2 + 2 == 4
+    stack = Stack()
+    assert stack.is_empty() == True
+
+    stack.push(1)
+    assert stack.is_empty() == False
+
+def test_stack_top() -> None:
+    stack = Stack()
+    stack.push(1)
+    stack.push(2)
+    assert stack.top() == 2
+    stack.pop()
+    assert stack.top() == 1
